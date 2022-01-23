7f3e686f7dfb0f6cfb54de3e0373de7171011dde
DKorir <dominic.korir@student.moringaschool.com>
2022-01-23 12:56:26 +0300

    initial commit add save test to save object into the account list

--- account_test.py
+++ account_test.py
@@ -10,6 +10,16 @@ class TestAccount(unittest.TestCase):
         self.assertEqual(self.new_account.password,"123456")
         self.assertEqual(self.new_account.email,"dk@gmail.com")
 
+    def test_save_account(self):
+        '''
+        test_save_account test case to test if the account object is saved into
+         the account list
+        '''
+        self.new_account.save_account() # saving the new account
+        self.assertEqual(len(Account.account_list),1) 
+
+
+
 if __name__ == '__main__':
     unittest.main()
 
