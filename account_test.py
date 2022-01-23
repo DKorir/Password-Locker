d3a1cb2a3e934d3605908536a923d48b92e2396e
DKorir <dominic.korir@student.moringaschool.com>
2022-01-23 10:17:26 +0300

    initial commit create account object

--- account_test.py
+++ account_test.py
@@ -1,1 +1,6 @@
 import unittest # Importing the unittest module
+from account import Account # Importing the account class
+class TestAccount(unittest.TestCase):
+    def setUp(self):
+       
+        self.new_account = Account("Domie","Korir","123456","domi@gmail.com") # create Account object
