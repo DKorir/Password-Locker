3e693bd65b894e395df9563787759f8851c22b8a
DKorir <dominic.korir@student.moringaschool.com>
2022-01-23 10:02:58 +0300

    initial add init methhod that alows us to add properties for new object

--- account.py
+++ account.py
@@ -1,5 +1,15 @@
+import email
+from typing_extensions import Self
+
+
 class Account:
     """
     Class that generates instances of users.
     """
     account_list = [] #empty account list
+    
+    def __init__(self,account_name,user_name,password,email):
+        self.account_name = account_name
+        self.user_name = user_name
+        self.password = password
+        self.email = email
