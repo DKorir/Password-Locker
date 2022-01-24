300b9f2a9660247e1348e834139ac171d09bbf9f
DKorir <dominic.korir@student.moringaschool.com>
2022-01-24 08:31:15 +0300

    create  credentials class and docstring for it that carries username credentials name, password and email

--- credentials.py
+++ credentials.py
@@ -3,4 +3,10 @@ class Credentials:
     class that generates new instances of users
     """
     credentials_list = [] #empty credentials list
-    
+
+    def __init__(self,credentials_name,user_name,
+    password,email):
+        self.credentials_name = credentials_name
+        self.usr_name = user_name
+        self.password = password
+        self.email = email
