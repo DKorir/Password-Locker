97b8584fc3617ecb9c47ef2c6148942bdd927b72
DKorir <dominic.korir@student.moringaschool.com>
2022-01-24 18:17:23 +0300

    initial coommit add function that checks the existence of credentials that checks the name of the credentials

--- run.py
+++ run.py
@@ -62,3 +62,9 @@ def find_credentials(name):
     Function that finds a account by nane and returns the account
     '''
     return Credentials.find_by_name(name)
+
+def check_existing_credentials(name):
+    '''
+    Function that check if an account exists with that name and return a Boolean
+    '''
+    return Credentials.credentials_exist(name) 
